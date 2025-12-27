2c3a35d6462dc24ee07ed00422d75c35e7133925
Almighty-Shogun <alm1ghtyshogun1998@gmail.com>
2025-12-27 02:12:13 +0100

    Feat: convert to primary constructor

--- AlmightyShogun.ConsoleCommands/ConsoleCommand.cs
+++ AlmightyShogun.ConsoleCommands/ConsoleCommand.cs
@@ -1,19 +1,10 @@
 namespace AlmightyShogun.ConsoleCommands;
 
-public class ConsoleCommand
+public class ConsoleCommand(string name, string description, string[] aliases, string usage, string? example)
 {
-    public string Name { get; }
-    public string Description { get; }
-    public string[] Aliases { get; }
-    public string? Usage { get; }
-    public string? Example { get; set; }
-
-    public ConsoleCommand(string name, string description, string[] aliases, string usage, string? example)
-    {
-        Name = name;
-        Description = description;
-        Aliases = aliases;
-        Usage = $"{name} {usage}";
-        Example = string.IsNullOrWhiteSpace(example) ? null : $"{name} {example}";
-    }
+    public string Name { get; } = name;
+    public string Description { get; } = description;
+    public string[] Aliases { get; } = aliases;
+    public string? Usage { get; } = $"{name} {usage}";
+    public string? Example { get; set; } = string.IsNullOrWhiteSpace(example) ? null : $"{name} {example}";
 }
